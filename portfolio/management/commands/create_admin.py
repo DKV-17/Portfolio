e7c86c965430d39b5bdee9adc44cbfcd86238199
Deepal <deepalece@gmail.com>
2026-08-20 09:27:12 +0530

    username creation

---
+++ portfolio/management/commands/create_admin.py
@@ -0,0 +1,44 @@
+import os
+
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+
+class Command(BaseCommand):
+    help = "Create the production admin user if it does not exist"
+
+    def handle(self, *args, **kwargs):
+
+        User = get_user_model()
+
+        username = os.getenv("DJANGO_ADMIN_USERNAME")
+        email = os.getenv("DJANGO_ADMIN_EMAIL")
+        password = os.getenv("DJANGO_ADMIN_PASSWORD")
+
+        if not username or not email or not password:
+            self.stdout.write(
+                self.style.WARNING(
+                    "Admin environment variables are not configured."
+                )
+            )
+            return
+
+        if User.objects.filter(username=username).exists():
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f"Admin user '{username}' already exists."
+                )
+            )
+            return
+
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password
+        )
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Production admin '{username}' created successfully."
+            )
+        )
